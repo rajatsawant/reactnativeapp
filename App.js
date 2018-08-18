0cff75aa2ccd33fd51502687c70f413e60974c14
Rajat Sawant <rajatsawant610@gmail.com>
2018-08-18 23:46:16 +0530

    justifyContent and alignItems

--- App.js
+++ App.js
@@ -4,10 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{flex:1,flexDirection: 'row'}}>
-        <View style={{flex: 1, backgroundColor:'red', height: 50}} ></View>
-        <View style={{flex: 2, backgroundColor:'yellow', height: 50}}></View>
-        <View style={{flex: 3, backgroundColor:'green', height: 50}}></View>
+      <View style={{
+          flex:1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems:'center'
+        }}>
+        <View style={{backgroundColor:'red', height: 50, width: 50}} ></View>
+        <View style={{backgroundColor:'yellow', height: 50, width: 50}}></View>
+        <View style={{backgroundColor:'green', height: 50, width: 50}}></View>
       </View>
     );
   }
