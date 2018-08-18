6e96ad2551b7c0e5fd3853f05274163380456bc8
Rajat Sawant <rajatsawant610@gmail.com>
2018-08-18 23:16:17 +0530

    State and props code

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class Greetings extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      isShowingText: true
+    }
+
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
   render() {
+    let display = this.state.isShowingText ? this.props.name : ''; 
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Text>{display}</Text>
       </View>
     );
+  }
+}
+
+export default class LotsOfGreetings extends React.Component {
+  render() {
+    return (
+      <View style = {styles.container}> 
+        <Greetings name = "Rajat" />
+        <Greetings name = "Sumedh" />
+      </View>
+    )
   }
 }
 
